f3a3c7190214e8cd06d78d3ad545b8454f4f0240
Varya1315 <varvaraa2005@mail.ru>
2024-04-05 23:18:21 +0300

    fix 4-6 fina

--- src/test/java/weather/springwea/SpringweaApplicationTest.java
+++ src/test/java/weather/springwea/SpringweaApplicationTest.java
@@ -6,22 +6,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest
-class SpringweaApplicationTests {
+class SpringweaApplicationTest {
 
-    @Autowired
-    private ApplicationContext context;
 
-    @Test
-    public void contextLoads() {
-        try {
-            // Проверяем, что контекст приложения загружается успешно,
-            // что означает, что все бины и конфигурации настроены правильно.
-            assertThat(context).isNotNull();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
+
+        @Autowired
+        private ApplicationContext context;
+
+        @Test
+        public void contextLoads() {
+            try {
+                // Проверяем, что контекст приложения загружается успешно,
+                // что означает, что все бины и конфигурации настроены правильно.
+                assertThat(context).isNotNull();
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw e;
+            }
         }
     }
-}
+
+
